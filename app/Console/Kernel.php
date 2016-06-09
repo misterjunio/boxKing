45a9833d3e83cd8bb13a64a93fb3d3e86db77b70
Mister Júnio <ruca.33.maia@gmail.com>
2016-06-09 13:41:01 +0100

    scheduler to clear old classes added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,29 +2,30 @@
 
 namespace App\Console;
 
+use App\Lesson;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Carbon\Carbon;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        // Commands\Inspire::class,
-    ];
+class Kernel extends ConsoleKernel {
+	/**
+		* The Artisan commands provided by your application.
+		*
+		* @var array
+		*/
+	protected $commands = [
+			// Commands\Inspire::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+	/**
+		* Define the application's command schedule.
+		*
+		* @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+		* @return void
+		*/
+	protected function schedule(Schedule $schedule) {
+		$schedule->call(function () {
+			$lessons = Lesson::where('start_at', '<=', Carbon::now()->subWeeks(4))->delete();
+		})->weekly()->sundays()->at('23:59');
+	}
 }
