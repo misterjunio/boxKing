6f606d579dd98ea35704253a92a41e16340a027a
Mister Júnio <ruca.33.maia@gmail.com>
2016-06-09 17:05:49 +0100

    payment records are cleared at start of each month

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Lesson;
+use App\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Carbon\Carbon;
@@ -27,5 +28,9 @@ class Kernel extends ConsoleKernel {
 		$schedule->call(function () {
 			$lessons = Lesson::where('start_at', '<=', Carbon::now()->subWeeks(8))->delete();
 		})->weekly()->sundays()->at('23:59');
+		
+		$schedule->call(function () {
+			$lessons = User::where('current_month_payment', true)->update(['current_month_payment' => false]);
+		})->monthlyOn(1, '00:01');
 	}
 }
